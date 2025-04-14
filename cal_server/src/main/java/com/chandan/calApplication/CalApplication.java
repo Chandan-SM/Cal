46b18e7cbca536a16ca6ce45588fd3b86baba004
Chandan SM <chandan.sm.dev@gmail.com>
2025-04-14 22:37:53 +0530

    Add dotenv support for environment variables and configure database properties

--- cal_server/src/main/java/com/chandan/calApplication/CalApplication.java
+++ cal_server/src/main/java/com/chandan/calApplication/CalApplication.java
@@ -2,12 +2,23 @@ package com.chandan.calApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        
+        // Set the environment variables to System properties
+        System.setProperty("dbName", dotenv.get("DB_NAME"));
+        System.setProperty("dbUsername", dotenv.get("DB_USERNAME"));
+        System.setProperty("dbUserPassword", dotenv.get("DB_USER_PASSWORD"));
+        System.setProperty("dbHost", dotenv.get("DB_HOST"));
+        System.setProperty("dbPort", dotenv.get("DB_PORT"));
 
+        // Start the Spring Boot application
+        SpringApplication.run(CalApplication.class, args);
+    }
 }
